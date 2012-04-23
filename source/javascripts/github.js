09ef9c8c72b77a8331061394e660e153c0aaceec
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-04-24 00:45:08 +0200

    fixes github links

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -3,26 +3,18 @@ var github = (function(){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
     }
     t.innerHTML = fragment;
   }
   return {
     showRepos: function(options){
-      $.getJSON({
-          url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
-        , type: 'jsonp'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
-        , success: function(data) {
-          var repos = [];
-          if (!data || !data.repositories) { return; }
-          for (var i = 0; i < data.repositories.length; i++) {
-            if (options.skip_forks && data.repositories[i].fork) { continue; }
-            repos.push(data.repositories[i]);
-          }
+      $.getJSON("https://api.github.com/users/"+options.user+"/repos?callback=?",
+        { error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
+        , success: function(repos) {
           repos.sort(function(a, b) {
-            var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
+            var aDate = new Date(a.updated_at).valueOf(),
+                bDate = new Date(b.updated_at).valueOf();
 
             if (aDate === bDate) { return 0; }
             return aDate > bDate ? -1 : 1;
